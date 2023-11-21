ab97713c97b842700519aa61b5404d8823d38286
Yazdan <yazdan.ghezelbash@gmail.com>
2023-11-21 22:29:28 +0330

    session 1

---
+++ 3.py
@@ -0,0 +1,89 @@
+ur=input('plz enter username:')
+password=input('plz enter password:')
+
+if (ur=='yazdan' and password=='123'):
+    print('welcome')
+else:
+    print('plz enter right info!!') 
+#___________________________
+# tedad = 2
+
+# while tedad<=100:
+#     print(tedad,end=' ')
+#     tedad=tedad+2
+#__________________________________
+
+# for i in range(1,101):
+#     print(i,end=' ')
+
+#__________________________________
+# for i in range(2,101,2):
+#     print(i,end=' ') 
+# print('\n\n')
+
+# for i in range(1,101,2):
+#     print(i,end=' ')
+#__________________________________
+# for i in range(101,1000,2):
+#     print(i,end=' ')
+
+#___________list1_______________________
+# customerName = []
+
+# print(type(customerName))
+# #insert a value to list
+# customerName.append('a')
+# customerName.append('b')
+# print(customerName)
+#___________list2_______________________   
+# customerName=['a','b','c','d']
+# #select
+# print(customerName[0]) 
+# print(customerName[-1])
+# customerName.insert(2,'ali')
+# print(customerName)
+#___________list3_______________________ 
+# score = [12,20,5,16,7,8,12]
+# #search item
+# print(len(score))
+
+# for i in range(0,len(score)):
+#     if score[i]>=10:
+        
+#         print(i,end=' ')
+#___________list4_______________________
+# fruits = ['apple','orange','pinapple','banana']
+
+# for item in fruits:
+#     print(item)
+#___________list5_______________________
+# fruits = ['apple','orange','pinapple','banana']
+
+# for i in range(0,len(fruits)):
+#     if fruits[i]=='banana':
+#         print('find! in',i,'location')
+#     else:
+#         print('not find!')    
+#_______________________________________
+# no=int(input('plz enter one no:'))
+
+# if (no%2==0):
+#     print('even')
+# else:
+#     print('odd')   
+#______________________________________________
+# score = [12,4,5,6,7,8,12]
+
+# for i in range(0,7):
+#     print(i,end=' ')
+#     print(score[i])
+#______________________________________________
+# score = [12,10,15,9,20,6,17]
+
+# #update items
+# score[2]=15.25
+# print(score)
+
+# del score[-1]
+# print(score)
+#______________________________________________
